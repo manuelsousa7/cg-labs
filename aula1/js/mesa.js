cc1abece6575a8ed19ff528e5040d32ca82b3166
Manuel Sousa <manuelsousamvs@gmail.com>
2017-09-21 23:49:48 +0100

    update

--- aula1/js/mesa.js
+++ aula1/js/mesa.js
@@ -1,14 +1,32 @@
+/*global THREE*/
+
 var camera, scene, renderer;
 
 function render() {
 	'use strict';
 	renderer.render(scene,camera);
+
+function createScene() {
+	'use strict';
+	scene = new THREE.Scene();
+	scene.add(new THREE.AxisHelper(10));
+}
+
+function createCamera() {
+	'use strict';
+	camera = new THREE.PerspectiveCamera( 70,window.innetWidth, window.innetHeight, 1,1000 );
+	camera.position.x = 50;
+	camera.position.y = 50;
+	camera.position.z = 50;
+	camera.lookAt(scene.position);
 }
 
 function init() {
     'use strict';
     renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(windows.innerWith, window.innetHeight);
+    renderer.setSize(window.innerWidth, window.innetHeight);
     document.body.appendChild(renderer.domElement);
+    createScene();
+    createCamera();
     render();
 }
